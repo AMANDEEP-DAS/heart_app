636a83edac7c1e144e1d9ae95db1a8e5e6d1a851
AMANDEEP-DAS <dasaman1997@gmail.com>
2021-09-21 23:13:55 +0530

    loading added

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:heart_app/screens/splash_screen.dart';
+
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+));

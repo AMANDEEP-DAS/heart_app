ca066786815b719b8d3f0496ea1ab53ff0e6c133
AMANDEEP-DAS <dasaman1997@gmail.com>
2021-09-22 11:16:21 +0530

    login screen done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,8 +14,8 @@ class LoginScreen extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: NetworkImage('https://i.pinimg.com/originals/2c/c7/84/2cc784186a00d725720c4da63d97ed90.gif'),
-                fit: BoxFit.cover
+                image: NetworkImage('https://s3-eu-west-1.amazonaws.com/cdn.nottinghamdriving.com/live/desktop/images/framework/heart-rate.gif'),
+                fit: BoxFit.contain
             )
         ),
         child: Stack(
@@ -33,45 +33,44 @@ class LoginScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('EXPLORE',style: TextStyle(fontSize: 45,color: Colors.white,fontWeight: FontWeight.bold),),
-                    Text('Your own Planet',style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),),
-                    Text('Rule the Universe',style: TextStyle(fontSize: 20,color: Colors.white),),
+                    Text('Redheart',style: TextStyle(fontSize: 45,color: Colors.red,fontWeight: FontWeight.bold),),
+                    Text('',style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),),
+                    Text('',style: TextStyle(fontSize: 20,color: Colors.white),),
                   ],
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.topRight,
-              child: GestureDetector(
-                onTap: (){
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>LoadingScreen()));
-                },
-                child: Container(
-                  padding: EdgeInsets.only(bottom: 30,left: 30),
-                  height: MediaQuery.of(context).size.height/7,
-                  width: MediaQuery.of(context).size.width/2,
-                  decoration: BoxDecoration(
-                    color: Color(0xff1fd1a5),
-                    borderRadius: BorderRadius.only(bottomLeft:Radius.circular(50)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade800.withOpacity(0.5),
-                        spreadRadius: 10,
-                        blurRadius: 10,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Skip',style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),)
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            // Align(
+            //   alignment: Alignment.topRight,
+            //   child: GestureDetector(
+            //     onTap: (){
+            //     },
+            //     child: Container(
+            //       padding: EdgeInsets.only(bottom: 30,left: 30),
+            //       height: MediaQuery.of(context).size.height/7,
+            //       width: MediaQuery.of(context).size.width/2,
+            //       decoration: BoxDecoration(
+            //         color: Color(0xff1fd1a5),
+            //         borderRadius: BorderRadius.only(bottomLeft:Radius.circular(50)),
+            //         boxShadow: [
+            //           BoxShadow(
+            //             color: Colors.grey.shade800.withOpacity(0.5),
+            //             spreadRadius: 10,
+            //             blurRadius: 10,
+            //             offset: Offset(0, 3), // changes position of shadow
+            //           ),
+            //         ],
+            //       ),
+            //       child: Column(
+            //         mainAxisAlignment: MainAxisAlignment.end,
+            //         crossAxisAlignment: CrossAxisAlignment.start,
+            //         children: [
+            //           Text('Skip',style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),)
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            // ),
             Align(
               alignment: Alignment.bottomLeft,
               child: Container(
@@ -79,7 +78,7 @@ class LoginScreen extends StatelessWidget {
                 height: MediaQuery.of(context).size.height/5,
                 width: MediaQuery.of(context).size.width/1.4,
                 decoration: BoxDecoration(
-                  color:  Color(0xff11018a),
+                  color:  Color(0xfff2870c),
                   borderRadius: BorderRadius.only(topRight:Radius.circular(50)),
                   boxShadow: [
                     BoxShadow(
@@ -101,28 +100,33 @@ class LoginScreen extends StatelessWidget {
             ),
             Align(
               alignment: Alignment.bottomRight,
-              child: Container(
-                padding: EdgeInsets.all(30),
-                height: MediaQuery.of(context).size.height/9,
-                width: MediaQuery.of(context).size.width/1.5,
-                decoration: BoxDecoration(
-                  color: Colors.deepOrangeAccent,
-                  borderRadius: BorderRadius.only(topLeft:Radius.circular(50)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade800.withOpacity(0.5),
-                      spreadRadius: 10,
-                      blurRadius: 10,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Sign In',style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),)
-                  ],
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>LoadingScreen()));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  height: MediaQuery.of(context).size.height/9,
+                  width: MediaQuery.of(context).size.width/1.5,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(topLeft:Radius.circular(50)),
+                    color: Color(0xff0ad1a9),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade800.withOpacity(0.5),
+                        spreadRadius: 10,
+                        blurRadius: 10,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Sign In',style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),)
+                    ],
+                  ),
                 ),
               ),
             )
